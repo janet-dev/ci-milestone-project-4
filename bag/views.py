bd1f753e1ad7bd2c9229dab9dc116e3b901bc66a
JD <janet_dornan@hotmail.com>
2023-05-29 13:27:15 +0000

    update context processor with bag quantity and items

--- bag/views.py
+++ bag/views.py
@@ -20,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])  # for testing shopping bag in session
     return redirect(redirect_url)
