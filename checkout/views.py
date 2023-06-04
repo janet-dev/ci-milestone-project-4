f89e687a65377fd1042892ef5f7c37952f8b997d
JD <janet_dornan@hotmail.com>
2023-06-04 12:45:03 +0000

    Add stripe elements to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NFElXDct0wQKx5oYYzZtRbzVSnZzUxH6ksr2hYyJEDcOVbYUGZeghg6eAQdxqOWs30h0MbHIlectbGddAI0JE1O00JbTS6vtT',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
